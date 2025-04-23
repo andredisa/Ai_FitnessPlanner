2927f7713651a5b5ac02c3ad352ebbd550da2693
andredisa <andreadisanti22@gmail.com>
2025-04-23 08:54:06 +0200

    oggetto per costruire il profilo utente

---
+++ models/user_profile.py
@@ -0,0 +1,10 @@
+def build_profile(age, weight, height, sex, activity_level, dietary_preferences, fitness_goals):
+    return f"""
+    Age: {age}
+    Weight: {weight}kg
+    Height: {height}cm
+    Sex: {sex}
+    Activity Level: {activity_level}
+    Dietary Preferences: {dietary_preferences}
+    Fitness Goals: {fitness_goals}
+    """
